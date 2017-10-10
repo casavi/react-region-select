979a72e2f311bb069385d26f48336649f394dd6a
mdewar <mdewar@nn4m.com>
2017-10-10 17:04:08 +0100

    revert server to port 3000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
-var port = process.env.PORT || 3050;
+var port = process.env.PORT || 3000;
 
 new WebpackDevServer(webpack(config), {
 	publicPath: config.output.publicPath,
